e6adee1f198de343b74d278f4a15fa1306d56549
Lev <lev01111989@gmail.com>
2025-08-18 11:33:16 +0200

    edit to orders/admin.py

---
+++ orders/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Order, OrderItem, Shipment
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "buyer", "seller", "status", "total", "currency", "placed_at")
+    list_filter = ("status",)
+    inlines = [OrderItemInline]
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ("order", "carrier", "tracking_number", "status")
